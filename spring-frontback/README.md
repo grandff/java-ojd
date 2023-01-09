4a29d5c872e3e13b404d68e240332e58e0c88f8d
grandff <dsaqaz421@naver.com>
2023-01-09 21:25:11 +0900

    Feat: init front back project

---
+++ spring-frontback/README.md
@@ -0,0 +1,17 @@
+# Spring front-back version
+
+## 개요
+
+front-end와 back-end를 나눈 Spring 웹시스템
+
+## 교육순서
+
+## 과제목록
+
+## 특이사항
+
+## 기본 기능
+
+## 최초 설정 및 구동 방법
+
+## 참고
---
+++ spring-frontback/front-end/src/test/java/kdn/spring/sample/frontend/FrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package kdn.spring.sample.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
